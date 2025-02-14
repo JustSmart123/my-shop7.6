3a9d735be10ea303299719cfdef0263217581525
Liliya Sviatenko <123566371+Liliya-ivanovna@users.noreply.github.com>
2025-02-14 11:16:46 +0200

    update

--- my-shop7.6/src/pages/AdminDashboard.vue
+++ my-shop7.6/src/pages/AdminDashboard.vue
@@ -20,7 +20,7 @@ export default {
 </script>
 
 <style scoped>
-/* Основний контейнер */
+
 .admin-panel {
   display: flex;
   flex-direction: column;
--- my-shop7.6/src/router.js
+++ my-shop7.6/src/router.js
@@ -1,33 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router';
-// import App from './App.vue'; 
 import AdminDashboard from './pages/AdminDashboard.vue';
 import ManageProducts from './pages/ManageProducts.vue';
 import ManageReviews from './pages/ManageReviews.vue';
 
 const routes = [
-    {
-      path: '/',
-      component: () => import('./App.vue'), // Головна сторінка
-    },
-    {
-      path: '/admin',
-      component: AdminDashboard, // Адмін-панель
-      children: [
-        {
-          path: 'products',
-          component: ManageProducts, // Керування продуктами
-        },
-        {
-          path: 'reviews',
-          component: ManageReviews, // Керування відгуками
-        },
-      ],
-    },
-  ];
+  {
+    path: '/',
+    component: AdminDashboard, // Використовуємо AdminDashboard як головний компонент
+    children: [
+      { path: '', component: ManageProducts }, // Домашня сторінка рендерить ManageProducts
+      { path: 'reviews', component: ManageReviews }, // Відгуки
+    ],
+  },
+  {
+    path: '/admin',
+    component: AdminDashboard,
+    children: [
+      { path: 'products', component: ManageProducts },
+      { path: 'reviews', component: ManageReviews },
+    ],
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/my-shop7.6/'), // Вказуємо базовий шлях для GitHub Pages
   routes,
 });
 
 export default router;
+
